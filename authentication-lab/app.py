a3d25491d8ad8e900d3b83ebacb6758c27f73e04
aidanfl23-meet <93254417+aidanfl23-meet@users.noreply.github.com>
2022-08-01 16:15:09 +0300

    Done

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -10,11 +10,12 @@ config = {
   "messagingSenderId": "115400578724",
   "appId": "1:115400578724:web:a8565930f311175a6db831",
   "measurementId": "G-G009ZDNMKG",
-  "databaseURL": ""
+  "databaseURL": "https://meet---2022-default-rtdb.europe-west1.firebasedatabase.app/"
 }
 
 firebase = pyrebase.initialize_app(config)
 auth = firebase.auth()
+db = firebase.database()
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
@@ -41,15 +42,24 @@ def signin():
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
+
     if request.method == "POST":
         email = request.form['email']
         password = request.form['password']
+
         try:
             login_session['user'] = auth.create_user_with_email_and_password(email, password)
+            name = request.form['full_name']
+            username = request.form['username']
+            bio = request.form['bio']
+            user = {"full name" : name , "username" : username , "biography" : bio}
+            db.child("Users").child(login_session['user']['localId']).set(user)
             return redirect(url_for('add_tweet'))
+
         except:
             error = "Error"
             return render_template("signup.html")
+
     else:
         return render_template("signup.html")
 
@@ -57,7 +67,27 @@ def signup():
 
 @app.route('/add_tweet', methods=['GET', 'POST'])
 def add_tweet():
+
+    if request.method == "POST":
+        title = request.form['title']
+        text = request.form['text']
+        #try: 
+        tweet = {"title" : title , "text" : text , "uid" : login_session['user']['localId']}
+        db.child("Tweets").push(tweet)
+        return redirect(url_for('all_tweets'))
+
+        #except:
+        #    return render_template("add_tweet.html")
+
+    else: 
+        return render_template("add_tweet.html")
+
     return render_template("add_tweet.html")
+
+@app.route('/all_tweets')
+def all_tweets():
+    all_the_tweets = db.child("Tweets").get().val().values()
+    return render_template("all_tweets.html", all_the_tweets = all_the_tweets )
 
 
 if __name__ == '__main__':
