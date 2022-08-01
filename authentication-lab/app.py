5db4ca829f36dc163a532f4a776805f45df64a08
aidanfl23-meet <93254417+aidanfl23-meet@users.noreply.github.com>
2022-08-01 09:14:56 +0300

    Done

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -2,18 +2,57 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from flask import session as login_session
 import pyrebase
 
+config = {
+  "apiKey": "AIzaSyASx2vWHHg418m4HX4eKbxOrIqC75zACo8",
+  "authDomain": "meet---2022.firebaseapp.com",
+  "projectId": "meet---2022",
+  "storageBucket": "meet---2022.appspot.com",
+  "messagingSenderId": "115400578724",
+  "appId": "1:115400578724:web:a8565930f311175a6db831",
+  "measurementId": "G-G009ZDNMKG",
+  "databaseURL": ""
+}
+
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
+
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
 
 
 @app.route('/', methods=['GET', 'POST'])
 def signin():
+    error = ""
+    if request.method == 'POST':
+       email = request.form['email']
+       password = request.form['password']
+       try:
+            login_session['user'] = auth.sign_in_with_email_and_password(email, password)
+            return redirect(url_for('add_tweet'))
+       except:
+           error = "Authentication failed"
+           return render_template("signup.html")
+    else:
+        return render_template("signin.html")
+
+
     return render_template("signin.html")
 
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    return render_template("signup.html")
+    if request.method == "POST":
+        email = request.form['email']
+        password = request.form['password']
+        try:
+            login_session['user'] = auth.create_user_with_email_and_password(email, password)
+            return redirect(url_for('add_tweet'))
+        except:
+            error = "Error"
+            return render_template("signup.html")
+    else:
+        return render_template("signup.html")
+
 
 
 @app.route('/add_tweet', methods=['GET', 'POST'])
